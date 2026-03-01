63b86954b1e98d03c0b56d00512d6ec5bf4d68b7
Joshua Bello <joshuabello2550@gmail.com>
2026-03-01 00:22:21 -0500

    adding the initial files, copied from lovable

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// import { PostHogProvider } from "@posthog/react";
 
-// const options = {
-//   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-//   defaults: "2026-01-30",
-// } as const;
-
-// console.log(
-//   "import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN: ",
-//   import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN
-// );
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN}
-      options={options}
-    > */}
     <App />
-    {/* </PostHogProvider> */}
   </StrictMode>
 );
