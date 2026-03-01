c7ae04178cc516d62a58a582541e9cd1d1c5abf0
GenericP3rson <genericp3rson@gmail.com>
2026-02-28 19:31:17 -0500

    add posthog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import usePostHog from './lib/usePostHog'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  usePostHog()
   const [count, setCount] = useState(0)
 
   return (
