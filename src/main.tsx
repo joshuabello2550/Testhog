8c24522fb6d75bdf895c7a4c959c179cc5fad73e
Joshua Bello <joshuabello2550@gmail.com>
2026-02-28 22:04:55 -0500

    commented out stuff that is not working

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { PostHogProvider } from "@posthog/react";
+// import { PostHogProvider } from "@posthog/react";
 
-const options = {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  defaults: "2026-01-30",
-} as const;
+// const options = {
+//   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+//   defaults: "2026-01-30",
+// } as const;
 
 console.log(
   "import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN: ",
@@ -15,11 +15,11 @@ console.log(
 );
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
+    {/* <PostHogProvider
       apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN}
       options={options}
-    >
-      <App />
-    </PostHogProvider>
+    > */}
+    <App />
+    {/* </PostHogProvider> */}
   </StrictMode>
 );
